ff6926f318db8da78d7cc8a7a09c907af691a474
Knij42 <goldendanik@mail.ru>
2023-11-13 16:43:52 +0300

    add BMR calc

--- GitTest/Pages/BMRPage.xaml.cs
+++ GitTest/Pages/BMRPage.xaml.cs
@@ -24,5 +24,64 @@ namespace GitTest.Pages
         {
             InitializeComponent();
         }
+        float g;
+        float v;
+        float r;
+        float a;
+        private void male(object sender, RoutedEventArgs e)
+        {
+            g = 66;
+            v = 13.7f;
+            r = 5;
+            a = 6.8f;
+            nfemale.Opacity = 0.7;
+            nmale.Opacity = 1;
+        }
+
+        private void female(object sender, RoutedEventArgs e)
+        {
+            g = 655;
+            v = 9.6f;
+            r = 1.8f;
+            a = 4.7f;
+            nmale.Opacity = 0.7;
+            nfemale.Opacity = 1;
+        }
+
+        private void Btn_Back(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void count(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float ves = float.Parse(t_v.Text);
+                float height = float.Parse(t_height.Text);
+                float age = float.Parse(t_age.Text);
+                float bmr = g + (ves * v) + (r * height) - (a * age);
+                if ((ves < 0 && height < 0 && age < 0) || (ves < 0 && height < 0) || (ves < 0 && age < 0) || (height > 0 && age < 0))
+                {
+                    MessageBox.Show("Указанны отрицательные значения");
+                }
+                else if (ves < 0 || age < 0 || height < 0)
+                {
+                    MessageBox.Show("Указанно отрицательное значение");
+                }
+                else if (g == 0)
+                {
+                    MessageBox.Show("Выбирите пол");
+                }
+                else
+                {
+                    bmr_tb.Text = bmr.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
